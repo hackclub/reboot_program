aa2148453521fbcf12112be27932c941d4fcf2b5
hardwarepro <92922224+scooterthedev@users.noreply.github.com>
2026-01-08 22:03:33 -0500

    revert shop purchasing code to fix it, and swap mouse images since they were flipped.

--- app/models/shop_order.rb
+++ app/models/shop_order.rb
@@ -1,12 +1,4 @@
 class ShopOrder < ApplicationRecord
   belongs_to :user
   belongs_to :shop_item
-
-  after_save :sync_to_airtable
-
-  private
-
-  def sync_to_airtable
-    Airtable::ShopOrderSyncJob.perform_later
-  end
 end
